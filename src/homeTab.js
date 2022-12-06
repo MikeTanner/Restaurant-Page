14059ed76e06c9934a7639547a69d514f21fd16c
Mike Tanner <miketannercs@gmail.com>
2022-12-05 19:10:23 -0600

    home tab content complete (Css missing)

--- src/homeTab.js
+++ src/homeTab.js
@@ -2,7 +2,9 @@ import missionStatementImg from "../assets/tacoMonster.gif"
 const create = (container) => {
     const title = titleDivMaker("Greg's Food Restaurant");
     const missionDiv = missionStatement()
-    container.append(title,missionDiv);
+    const hoursDiv = hoursOfOperation();
+    const locationDiv = location();
+    container.append(title,missionDiv, hoursDiv, locationDiv);
 }
 const titleDivMaker = (content) =>{
     const titleDiv = document.createElement("p");
@@ -22,7 +24,33 @@ const missionStatement = () => {
     missionSig.innerHTML = "Greg."
     missionDiv.append(missionText,missionSig,missionImg)
     return missionDiv;
-
 }
+
+const hoursOfOperation = () => {
+    const container = document.createElement("div")
+    container.classList.add("schedule")
+    const smallTitle = document.createElement("h2")
+    smallTitle.innerHTML = "Hours"
+    container.appendChild(smallTitle)
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    days.forEach(((item) => {
+        const content = document.createElement("p")
+        content.innerHTML = item + ": 8am-8pm";
+        container.appendChild(content);
+    }))
+    return container;
+}
+const location = () => {
+    const container = document.createElement("div")
+    container.classList.add("location")
+    const smallTitle = document.createElement("h2")
+    smallTitle.innerHTML = "Location"
+    const content = document.createElement("p")
+    content.innerHTML = "313 Greg Zone, Gregland, Ohio";
+    container.append(smallTitle, content)
+
+    return container;
+}
+
 
 export {create}
